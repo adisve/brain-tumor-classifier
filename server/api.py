dc826ac71569fdf5fd78056bc9ec18ad19abbb8f
Adis Veletanlic <96535657+adisve@users.noreply.github.com>
2023-10-12 19:01:22 +0200

    Add backend and small website to use model

---
+++ server/api.py
@@ -0,0 +1,39 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from utils import BrainTumorClassifier, augment_image_cv2
+from fastapi import FastAPI
+from pathlib import Path
+import numpy as np
+import cv2
+
+classifier = BrainTumorClassifier()
+
+labels = ['glioma', 'meningioma', 'no_tumor', 'pituitary']
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    file_bytes = np.asarray(bytearray(await file.read()), dtype=np.uint8)
+    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    image = augment_image_cv2(image)
+
+    array = np.expand_dims(image, axis=0)
+    prediction = classifier.model.predict(array)
+    label = np.argmax(prediction)
+
+    return {"label": str(label)}
+
+@app.get('/')
+async def index():
+    return FileResponse(Path("static/index.html"))
---
+++ server/utils.py
@@ -0,0 +1,20 @@
+from tensorflow import keras
+import cv2
+
+class BrainTumorClassifier():
+    def __init__(self) -> None:
+        self.model = self.load_model(model_path='../model/model.keras')
+
+    def load_model(self, model_path):
+        try:
+            return keras.models.load_model(model_path)
+        except OSError:
+            return None
+
+
+def augment_image_cv2(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.bilateralFilter(image, 2, 50, 50)
+    image = cv2.applyColorMap(image, cv2.COLORMAP_BONE)
+    image = cv2.resize(image, (200, 200))
+    return image
